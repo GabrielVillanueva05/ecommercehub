a435f82fd24424937a76b631d8fcd06693dfbcfa
GabrielVillanueva1 <gvillanueva2023@fau.edu>
2026-05-05 14:25:17 -0400

    Styling for SignUp Page

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,10 +1,51 @@
 import React from "react";
+import { useState } from "react";
+import "./Signup.css";
 
 const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSignup(e) {
+    e.preventDefault();
+
+    console.log("Signup info:", {
+      email,
+      password,
+    });
+  }
+
   return (
-    <div>
-      <h1>Sign Up</h1>
-    </div>
+    <>
+      <div className="signup-page-container">
+        <div className="signup-container">
+          <h1 className="signup-title">Sign Up</h1>
+          <form onSubmit={handleSignup}>
+            <div>
+              <label>Email:</label>
+              <input
+                type="email"
+                placeholder="Enter your email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+
+            <div>
+              <label>Password:</label>
+              <input
+                type="password"
+                placeholder="Create a password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+
+            <button type="submit">Sign Up</button>
+          </form>
+        </div>
+      </div>
+    </>
   );
 };
 
